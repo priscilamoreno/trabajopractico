0ec12bde82802c21aaddb7a66bc0aed1e8637633
priscilamo0reno <prismoreno12@gmail.com>
2019-10-30 20:51:27 -0300

    suma.py

---
+++ suma.py
@@ -0,0 +1,9 @@
+import unittest
+import sut
+class TestSut(unittest.TestCase):
+    def testuno(self):
+        def test_suma(self):
+            a=sut.sumar(a,b)
+            self.assertEqual(a + b)
+if __name__=="__main__":
+    unittest.main()
