7d7de23b30a4fe23574fd1b3e5c99e26082bbd5d
priscilamo0reno <prismoreno12@gmail.com>
2019-10-30 20:49:46 -0300

    saluda.py

---
+++ saluda.py
@@ -0,0 +1,10 @@
+import unittest
+import sut
+class TestSut(unittest.TestCase):
+    def testuno(self):
+        def test_saluda(self):
+            a=sut.saludar(nombre)
+            self.assertEqual(saluda + nombre)
+if __name__=="__main__":
+    unittest.main()
+        
