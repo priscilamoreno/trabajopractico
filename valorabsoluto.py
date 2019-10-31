5ff5be741584a2b878a262a6f9a0c59a2eb5fa61
priscilamo0reno <prismoreno12@gmail.com>
2019-10-31 12:06:23 -0300

    valorabsoluto.py

---
+++ valorabsoluto.py
@@ -0,0 +1,9 @@
+import unittest
+import sut
+class TestSut(unittest.TestCase):
+    def testuno(self):
+        def test_valorabsoluto(self):
+            a=sut.valorabsoluto(n)
+            self.assertEqual(n<0)
+if __name__=="__main__":
+    unittest.main()
