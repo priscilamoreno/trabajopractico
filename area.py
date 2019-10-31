1c4ca74112bcb705b75fd694483a87ecffcb2f3d
priscilamo0reno <prismoreno12@gmail.com>
2019-10-31 11:51:06 -0300

    area.py

---
+++ area.py
@@ -0,0 +1,10 @@
+import unittest
+import sut
+class TestSut(unittest.TestCase):
+    def testuno(self):
+        def test_area(self):
+            area=sut.area(3,2)
+            self.assertTrue(area==6)
+if __name__=="__main__":
+    unittest.main()
+        
